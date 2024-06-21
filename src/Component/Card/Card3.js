ee1d51c113e32cfa4eb40e128109bd9be8281057
shabbeer <mdshabbeer707@gmail.com>
2024-06-21 17:35:31 +0530

    backend

--- src/Component/Card/Card3.js
+++ src/Component/Card/Card3.js
@@ -16,12 +16,17 @@ const Card3 = ({ deleteItem, note, updateNote, showAlert }) => {
         updateNote(note);
     };
 
+    const getTruncatedDescription = (description) => {
+        const words = description.split(" ");
+        return words.length > 50 ? words.slice(0, 50).join(" ") + "..." : description;
+    };
+
     return (
         <div className="col-md-3" style={{ color: "black" }}>
             <div className="card">
                 <div className="card-title">
                     <h5>{note.name}</h5>
-                    <p>{note.description}</p>
+                    <p>{getTruncatedDescription(note.description)}</p>
                 </div>
                 <div className="card-buttons">
                     <p onClick={handleDelete} className="icon-button">
